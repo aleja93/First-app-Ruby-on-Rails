23877eea86df0fe43d57a2e4dfb3c0c9e23f61d4
Alejandra Ponce <aledennis.93@gmail.com>
2016-11-19 16:36:47 -0500

    Add Heroku

--- app/models/ice_cream.rb
+++ app/models/ice_cream.rb
@@ -1,2 +1,3 @@
 class IceCream < ActiveRecord::Base
+  mount_uploader :picture, PictureUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
   resources :ice_creams
+  root :to => redirect('/ice_creams')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
